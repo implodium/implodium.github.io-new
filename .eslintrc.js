8020d6e76d8c36b119eabfbb960d026445933a32
Quirin <quirin.ecker@bajupa.com>
2020-08-08 03:57:10 +0200

    updated the eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': "off",
+    'class-methods-use-this': "off",
+    'indent': ["error", 4],
+    'lines-between-class-members': "off",
+    'no-trailing-spaces': "off",
+    'arrow-parens': "off"
   },
 };
