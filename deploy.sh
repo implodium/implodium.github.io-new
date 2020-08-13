6ca5e06b4ed3942dcdb769d5c2aa338049747d53
Quirin <quirin.ecker@bajupa.com>
2020-08-13 16:55:07 +0200

    fixed github action build script

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,3 @@ cp -r dist docs
 sed -i -e 's/src=\//src=/g' docs/index.html
 sed -i -e 's/href=\//href=/g' docs/index.html
 rm -rf dist
-
-git add *
-git commit -m "$1"
-git push origin master
-
-# Object asdiofj = Object();
