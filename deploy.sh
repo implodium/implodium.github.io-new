95d897b89a2c84f2b342ce2de1396349da06664d
Quirin <quirin.ecker@bajupa.com>
2020-08-13 21:35:40 +0200

    testing the new deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,8 @@ cp -r dist docs
 sed -i -e 's/src=\//src=/g' docs/index.html
 sed -i -e 's/href=\//href=/g' docs/index.html
 rm -rf dist
-touch helloworld.txt
+touch helloWorld.txt
+
+git add *
+git commit --amend
+git push
