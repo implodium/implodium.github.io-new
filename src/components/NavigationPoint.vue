6633d54e63ad6d05899f4c2a3a9ad98be917f577
Quirin <quirin.ecker@bajupa.com>
2020-08-26 17:23:15 +0200

    set up navigation and content on the main page

---
+++ src/components/NavigationPoint.vue
@@ -0,0 +1,17 @@
+<template>
+<main>
+    Menu Point
+</main>
+</template>
+
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to the Implodium Page"/>
-    </div>
+    <div> Content </div>
 </template>
 
 <script lang="ts">
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import NavigationPoint from '@/components/NavigationPoint.vue'
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
     components: {
         HelloWorld,
+        NavigationPoint,
     },
 })
 export default class Home extends Vue {
