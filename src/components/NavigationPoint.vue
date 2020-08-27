7c41bbd005c90d562d136d93154d400bade53bd2
Quirin <quirin.ecker@bajupa.com>
2020-08-27 03:15:28 +0200

    added hover animation

--- src/components/NavigationPoint.vue
+++ src/components/NavigationPoint.vue
@@ -1,6 +1,10 @@
 <template>
-    <div :id="`navigation-point-${this.displayId}`" class="navigation-field navigation-point">
-        <div> {{ this.displayName }} </div>
+    <div :id="`navigation-point-${this.displayId}`"
+         class="navigation-field navigation-point"
+         @mouseover="hoverOn"
+         @mouseout="hoverOff">
+        <span> {{ this.displayName }} </span>
+        <div class="animation-box"></div>
     </div>
 </template>
 
@@ -21,15 +25,50 @@ export default class NavigationPoint extends NavigationField {
     protected get displayId(): string {
         return this.name.toLowerCase();
     }
+
+    mounted() {
+        const animationBox = document.querySelector(
+            `#navigation-point-${this.displayId} .animation-box`,
+        ) as HTMLElement
+
+        animationBox.style.width = this.width
+    }
+
+    private hoverOn() {
+        const animationBox = document.querySelector(
+            `#navigation-point-${this.displayId} .animation-box`,
+        ) as HTMLElement
+
+        animationBox.style.height = '1vh'
+    }
+
+    private hoverOff() {
+        const animationBox = document.querySelector(
+            `#navigation-point-${this.displayId} .animation-box`,
+        ) as HTMLElement
+
+        animationBox.style.height = '0'
+    }
 }
 </script>
 
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap');
 
-.navigation-point div {
-    font-family: 'Oswald', sans-serif;
-    font-size: 3vh;
+.navigation-point {
+
+    span {
+        font-family: 'Oswald', sans-serif;
+        font-size: 3vh;
+    }
+
+    .animation-box {
+        position: absolute;
+        align-self: flex-end;
+        height: 0;
+        background-color: #EE4950;
+        transition: height 0.5s;
+    }
 }
 
 </style>
