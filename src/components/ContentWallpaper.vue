836933c55e84f8b800764327669e06938eae3bfa
Quirin <quirin.ecker@bajupa.com>
2020-08-28 01:35:25 +0200

    started implementing the ContentWallpaper component

---
+++ src/components/ContentWallpaper.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="content-wallpaper"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ContentWallpaper extends Vue {
+    private static count = 0
+    private static wallpaperUrl = '/images/implo_wallpaper.png'
+    public static readonly componentName = 'content-wallpaper'
+    private readonly id = ContentWallpaper.count
+
+    created() { ContentWallpaper.count += 1 }
+
+    get elementId() {
+        return `${ContentWallpaper.componentName}-${this.id}`
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
