5230ab25780bc34092bd87fc8f233d3816725b54
Quirin <quirin.ecker@bajupa.com>
2020-08-28 01:46:29 +0200

    fixed bug with the shellscript and removed line for debugging

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-npm run build &&
+npm run vue-build &&
 rm -rf docs
 cp -r dist docs
 sed -i -e 's/src=\//src=/g' docs/index.html
 sed -i -e 's/href=\//href=/g' docs/index.html
 rm -rf dist
-touch helloWorld.txt
--- publish.sh
+++ publish.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-npm run build &&
+npm run vue-build &&
 rm -rf docs
 cp -r dist docs
 sed -i -e 's/src=\//src=/g' docs/index.html
 sed -i -e 's/href=\//href=/g' docs/index.html
 rm -rf dist
-touch helloWorld.txt
 
 git add *
 git commit -m 'publish the webpage'
