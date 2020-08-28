7411a74903600b70c2ee8b50642d2bd488436799
Quirin <quirin.ecker@bajupa.com>
2020-08-28 02:13:01 +0200

    now the navigation is on top of the content and won't disappear when scrolling

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,12 @@
 <template>
-    <div id="navigation-content">
-        <NavigationPoint name="Software"/>
-        <NavigationPoint name="Gaming"/>
-        <NavigationImage name="logo" url="/images/ImplodiumLogo.png"/>
-        <NavigationPoint name="About"/>
-        <NavigationPoint name="Discord"/>
+    <div class="navigation">
+        <div id="navigation-content">
+            <NavigationPoint name="Software"/>
+            <NavigationPoint name="Gaming"/>
+            <NavigationImage name="logo" url="/images/ImplodiumLogo.png"/>
+            <NavigationPoint name="About"/>
+            <NavigationPoint name="Discord"/>
+        </div>
     </div>
 </template>
 
@@ -24,11 +26,17 @@ export default class Navigation extends Vue {}
 
 <style scoped lang="scss">
 
-#navigation-content {
+.navigation {
+    width: 100vw;
+    height: 8vh;
+}
+
+.navigation #navigation-content {
     width: 100vw;
     height: 8vh;
     background-color: white;
     box-shadow: 0 10px 8px #888888;
+    position: fixed;
 
     display: flex;
     justify-content: space-evenly;
