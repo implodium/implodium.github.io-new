d42765c612a3f5ff5c5440a13438be41b43d2a92
Quirin <quirin.ecker@bajupa.com>
2020-08-30 16:34:00 +0200

    implemented new publish script

--- publish.sh
+++ publish.sh
@@ -7,6 +7,18 @@ sed -i -e 's/src=\//src=/g' docs/index.html
 sed -i -e 's/href=\//href=/g' docs/index.html
 rm -rf dist
 
+cd docs
+git init
 git add *
-git commit -m 'publish the webpage'
-git push
+git commit -m 'update webpage'
+
+echo 'Username: '
+read username
+echo 'Password: '
+read -s password
+
+git remote add origin https://$username:$password@github.com/implodium/implodium.github.io
+git branch gh-page
+git push --force origin gh-page
+cd ..
+rm -rf docs
