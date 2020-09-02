b94c523c31828776e03b4c21236c4942e01846ea
Quirin <quirin.ecker@bajupa.com>
2020-09-03 00:11:30 +0200

    credentials will be saved in a seperate file

--- publish.sh
+++ publish.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 npm run vue-build &&
 rm -rf docs
 cp -r dist docs
@@ -7,16 +6,32 @@ sed -i -e 's/src=\//src=/g' docs/index.html
 sed -i -e 's/href=\//href=/g' docs/index.html
 rm -rf dist
 
+
+
+if [ -e './git_credentials.sh' ]
+then
+  source git_credentials.sh
+else
+  read -p "Username: " username
+  read -p "Password: " -s password
+
+  echo "
+    #!/bin/sh
+    export username='$username'
+    export password='$password'
+  " > git_credentials.sh
+fi
+
+
+
 cd docs
+pwd
 git init
 git add *
 git commit -m 'update webpage'
 
-read -p "Username: " username
-read -p "Password: " -s password
-
-git remote add origin https://$username:$password@github.com/implodium/implodium.github.io
+git remote add gh-page https://$username:$password@github.com/implodium/implodium.github.io
 git branch gh-page
-git push --force origin gh-page
+git push --force gh-page gh-page
 cd ..
 rm -rf docs
